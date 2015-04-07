255eaba39bd0359fe8c10980b480a999710b95b5
vyara <vyara_hristova@yahoo.de>
2015-04-07 15:47:07 +0300

    vyara: added custom exception class and global error messages

---
+++ WPF-OOPTeam/EventScheduler.Data/Exceptions/Validator.cs
@@ -0,0 +1,32 @@
+﻿namespace EventScheduler.Data.Exceptions
+{
+    using System;
+
+
+    public static class Validator
+    {
+        public static void CheckIfObjectIsNull(object obj, string message = null)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException(message);
+            }
+        }
+
+        public static void CheckIfNullOrEmpty(string text, string message = null)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new NullReferenceException(message);
+            }
+        }
+
+        public static void CheckIfLengthIsValid(string text, int min = 0, int max, string message = null)
+        {
+            if (text.Length < min || max < text.Length)
+            {
+                throw new ArgumentOutOfRangeException(message);
+            }
+        }
+    }
+}
